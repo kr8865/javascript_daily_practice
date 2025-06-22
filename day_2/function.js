462df87549fbc0194b7ecf87c6321c312745c3db
kr8865 <misskratikaagrawal@gmail.com>
2025-06-22 14:21:20 +0530

    javascipt2nd day

---
+++ day_2/function.js
@@ -0,0 +1,75 @@
+//
+function sum(){
+    let num1=10;
+    let num2=30;
+    console.log(num1+num2);
+} //function definition 
+
+sum(); //function calling
+
+//prameterised functions
+//no definition needed like let const etc
+function sum1(num1,num2){
+    console.log(num1+num2);
+}
+sum1(10,20,30); //arguments
+
+function sum3(){
+    let num1=1;
+    let num2=2;
+    return num1+num2; //returned function
+}
+let val=sum3(); //catch here
+console.log(val);
+
+//function always return something-- undefined if not returned anything
+console.log(sum()); //undefined
+
+
+function val1(num1,num2){
+    console.log(num1);
+    console.log(num2); //undefined 
+    return num1+num2;//not a number
+
+}
+console.log(val1(10));//not a number
+//if js has any variable that does have any values js assign it to undefined
+//undefined + number gives you not a number
+
+//default parameterised function
+function sam(num1,num2=40){//defaltu parameter
+    console.log(num1);
+    console.log(num2);
+}
+sam(20);
+sam(20,40);// in this case ot will use passed value
+//overiding take place
+
+function gradecalc(marks){
+    if(marks>=90){
+        return "A";
+    }
+    else if(marks>=60){
+        return "B";
+    }
+    else if(marks>=40){
+        return "B+";
+    }
+    else{
+        return "fail";
+    }
+
+}
+gradecalc(45);
+
+function summ(num1,num2){
+    return num1
+    +num2;
+}
+let ans=summ(10,"sam");
+typeof(ans);
+console.log(summ(10,"sam")); //typecasting
+console.log(summ(10,true));
+console.log(summ("krati",true))
+
+
