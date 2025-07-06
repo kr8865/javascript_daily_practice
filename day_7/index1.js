a018578f45054e7337b4700d41998409b719f31e
kr8865 <misskratikaagrawal@gmail.com>
2025-07-06 11:16:45 +0530

    js

--- day_7/index1.js
+++ day_7/index1.js
@@ -7,7 +7,19 @@
 //these mathods to bring delay in function
 
 //but js m koi trika ni h js ko rokne ka
+setTimeout(() => {
+    
+}, timeout);
 setTimeout(function f(){
     console.log("gi");
 
 },5000);
+
+//location,local Storage,time outerHeight,setInterval,url,console(browser methods)
+//4 sec k lie callback queue k andr jata h joh vhn store rhta h 4 sec tkk  --fr jata h event loop k andr--fr vhn se call stack m jata h
+//callbackqueue-- webapi--superpower of browser
+//phle voh store kra leta h browser -- jb api bulate h tb--
+//jitna intezaar krna h utna kiya then usko bhj dega callback queue
+//callback quque-
+//event loop -check krta h ki mer call stack khali h --function ko bhj dega or if khali ni h toh yeh loop chlta rhega
+//
---
+++ day_8/index.js
@@ -0,0 +1,79 @@
+//callback k andr callback --calback hell or hell of dom
+//code grow horizontally --this makes js slow
+//2nd diadv---step 1 chalaya hi ni step 2 kbhi ni chlegaa --dependency jada hti h
+
+//callback --promises-async-await
+
+/////// ----promises---(solution of callback hell)
+
+/*function selectimage(){
+    setTimeout(function(){
+        console.log("select")
+    },3000)
+};
+selectimage();
+
+let promise=new Promise(function(resolve,reject){
+    setTimeout(function(){
+        let data="hi meri jaan";
+        resolve(data);
+        
+
+
+    },3000)
+});//returns a promise
+promise.then(function(data){
+   console.log(data);
+
+}).catch(function(ops){
+    console.log(ops);
+})
+*/
+/* let p1=new Promise((resolve,reject)=>{
+    console.log("kaam chl rha h");
+    setTimeout(()=>{
+        resolve();
+
+    },3000)
+
+})
+p1.then(()=>{
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            console.log(" h gya run");
+            resolve();
+        })
+        
+    },5000)
+
+}).then(()=>{
+    console.log("kaam h gye mere dono");
+})
+//vertical growth h ri h--.then() ki chaining h ri h jada hii bahut
+*/
+
+//async-await 
+async function k(){
+    await new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            console.log("3 sec h gye");
+            resolve();
+        },3000)
+
+    })
+    await new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            console.log("frse 3 h gye");
+            resolve();
+
+        },3000)
+
+    })
+    console.log("dono kaam h gye");
+
+};
+k();//returnig a promise
+//async returns a promise always
+
+//async always work with await 
+
