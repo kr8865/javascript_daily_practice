ac4f2d72ee1904c2f2565323e4be409fe0ba92ec
kr8865 <misskratikaagrawal@gmail.com>
2025-07-07 12:19:18 +0530

    day_9

---
+++ day_9/indx.js
@@ -0,0 +1,9 @@
+console.log("hi");
+console.log(document.getElementsByTagName('h1')); //it returns me a array
+//setinterval-rpeat itslf after particular interval of time
+setInterval(() => {
+    console.log("hi");
+}, 3000);
+
+let h=document.getElementsByTagName('h1');
+h.setAttribute("class","radha");
